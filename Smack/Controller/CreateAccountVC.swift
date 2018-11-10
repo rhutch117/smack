57381e99333d2b4c2f48c5927e9c7f253180576c
Ryan Hutchison <rwhutchison@live.com>
2018-11-09 23:34:34 -0600

    setup create account ui

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,22 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Ryan Hutchison on 11/9/18.
+//  Copyright © 2018 Ryan Hutchison. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+}
--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -18,4 +18,7 @@ class LoginVC: UIViewController {
     @IBAction func closedPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    @IBAction func createAccountBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
 }
