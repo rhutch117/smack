083bc9380e3ae369c093f1577f2b4a094e93b93b
Ryan Hutchison <rwhutchison@live.com>
2018-11-09 22:29:50 -0600

    Setup swreveal controller

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChannelVC.swift
 //  Smack
 //
 //  Created by Ryan Hutchison on 11/9/18.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.width - 60
     }
-
-
 }
-
