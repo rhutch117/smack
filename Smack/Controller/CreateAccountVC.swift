077c30f88e379b6b39a08e11cd8c6dc8f73ed76b
Ryan Hutchison <rwhutchison@live.com>
2018-11-10 20:07:07 -0600

    Allow registering of new users

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passTxt.text, passTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(withEmail: email, andPassword: pass) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
     }
     
     @IBAction func closePressed(_ sender: Any) {
