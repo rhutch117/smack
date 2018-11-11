fe1f5edc760753753d0d9c859a6e928206b57c65
Ryan Hutchison <rwhutchison@live.com>
2018-11-10 20:13:46 -0600

    Round button corners

---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Ryan Hutchison on 11/10/18.
+//  Copyright © 2018 Ryan Hutchison. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
