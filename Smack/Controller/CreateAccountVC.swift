502e75391c24d8550140200f69f11ac44e9c9c04
Ryan Hutchison <rwhutchison@live.com>
2018-11-11 16:32:01 -0600

    Clean up some code

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -23,8 +23,6 @@ class CreateAccountVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
