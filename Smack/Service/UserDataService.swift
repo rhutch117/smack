9473773cf1da5730379d5ea269964bbd6ec59968
Ryan Hutchison <rwhutchison@live.com>
2018-11-11 22:00:30 -0600

    Color avatar on channel controller

--- Smack/Service/UserDataService.swift
+++ Smack/Service/UserDataService.swift
@@ -30,4 +30,32 @@ class UserDataService {
     func setAvatarName(avatarName: String) {
         self.avatarName = avatarName
     }
+    
+    func returnUIColor(components: String) -> UIColor {
+        let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a: NSString?
+        
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        
+        guard let rUnwrapped = r else { return defaultColor }
+        guard let gUnwrapped = b else { return defaultColor }
+        guard let bUnwrapped = g else { return defaultColor }
+        guard let aUnwrapped = a else { return defaultColor }
+        
+        let rFloat = CGFloat(rUnwrapped.doubleValue)
+        let gFloat = CGFloat(gUnwrapped.doubleValue)
+        let bFloat = CGFloat(bUnwrapped.doubleValue)
+        let aFloat = CGFloat(aUnwrapped.doubleValue)
+        
+        return UIColor(red: rFloat, green: gFloat, blue: bFloat, alpha: aFloat)
+    }
 }
