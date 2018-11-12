5bd356c99f909db2731cb1225d0c41e609a83b49
Ryan Hutchison <rwhutchison@live.com>
2018-11-11 21:11:55 -0600

    Touch up create user ui

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Ryan Hutchison on 11/11/18.
+//  Copyright © 2018 Ryan Hutchison. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+}
